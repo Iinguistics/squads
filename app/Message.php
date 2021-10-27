4a403414d93873edb8d895aef349cdc6fa8eaea5
James <Thylinguistics@gmail.com>
2021-10-26 17:46:56 -0700

    building profile logic

---
+++ app/Message.php
@@ -0,0 +1,22 @@
+<?php
+
+//namespace App\Models;
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+
+    protected $table = "messages";
+    protected $primaryKey = 'message_id';
+
+    protected $fillable = [
+        'id', 'body', 'message_read', 'sent_from_id', 'created_at'
+    ];
+
+    public function user()
+    {
+        return $this->hasOne('App\User', 'id', 'id');
+    }
+}
