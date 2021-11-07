d173749f94ce740f5754bf2b85006c87308a9c35
James <Thylinguistics@gmail.com>
2021-11-07 12:05:00 -0800

    building messaging logic

--- public/js/app.js
+++ public/js/app.js
@@ -6683,15 +6683,86 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e2) { throw _e2; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e3) { didErr = true; err = _e3; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 
 
 
 var Conversation = function Conversation(_ref) {
   var conversationMessages = _ref.conversationMessages,
       sentFromProfile = _ref.sentFromProfile;
-  (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(function () {}, []);
+
+  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)([]),
+      _useState2 = _slicedToArray(_useState, 2),
+      messages = _useState2[0],
+      setMessages = _useState2[1];
+
+  var sortMessages = function sortMessages() {
+    var temp = [];
+
+    if (conversationMessages) {
+      if (conversationMessages[0]) {
+        var _iterator = _createForOfIteratorHelper(conversationMessages[0]),
+            _step;
+
+        try {
+          for (_iterator.s(); !(_step = _iterator.n()).done;) {
+            var message = _step.value;
+            temp.push(message);
+          }
+        } catch (err) {
+          _iterator.e(err);
+        } finally {
+          _iterator.f();
+        }
+      }
+
+      if (conversationMessages[1]) {
+        var _iterator2 = _createForOfIteratorHelper(conversationMessages[1]),
+            _step2;
+
+        try {
+          for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+            var _message = _step2.value;
+            temp.push(_message);
+          }
+        } catch (err) {
+          _iterator2.e(err);
+        } finally {
+          _iterator2.f();
+        }
+      }
+
+      temp.sort(function (a, b) {
+        var keyA = new Date(a.updated_at),
+            keyB = new Date(b.updated_at); // Compare the 2 dates
+
+        if (keyA < keyB) return 1;
+        if (keyA > keyB) return -1;
+        return 0;
+      });
+      setMessages(temp);
+    }
+  };
+
+  (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(function () {
+    sortMessages();
+  }, [conversationMessages]);
   console.log(conversationMessages, "convo");
   console.log(sentFromProfile, "sent from profile");
+  console.log(messages, "messages");
   return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {
     className: "",
     children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("h2", {
--- resources/js/components/Inbox/Conversation.js
+++ resources/js/components/Inbox/Conversation.js
@@ -1,10 +1,40 @@
 import React, { useEffect, useState } from "react";
 
 const Conversation = ({ conversationMessages, sentFromProfile }) => {
-    useEffect(() => {}, []);
+    const [messages, setMessages] = useState([]);
+
+    const sortMessages = () => {
+        let temp = [];
+        if (conversationMessages) {
+            if (conversationMessages[0]) {
+                for (let message of conversationMessages[0]) {
+                    temp.push(message);
+                }
+            }
+            if (conversationMessages[1]) {
+                for (let message of conversationMessages[1]) {
+                    temp.push(message);
+                }
+            }
+            temp.sort(function (a, b) {
+                var keyA = new Date(a.updated_at),
+                    keyB = new Date(b.updated_at);
+                // Compare the 2 dates
+                if (keyA < keyB) return 1;
+                if (keyA > keyB) return -1;
+                return 0;
+            });
+
+            setMessages(temp);
+        }
+    };
+    useEffect(() => {
+        sortMessages();
+    }, [conversationMessages]);
 
     console.log(conversationMessages, "convo");
     console.log(sentFromProfile, "sent from profile");
+    console.log(messages, "messages");
 
     return (
         <div className="">
