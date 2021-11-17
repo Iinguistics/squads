6c20bfcc9b82cf4800d2f017705e88702a6f1084
James <Thylinguistics@gmail.com>
2021-11-16 22:22:49 -0800

    building squads logic

--- app/Http/Controllers/SquadController.php
+++ app/Http/Controllers/SquadController.php
@@ -43,26 +43,35 @@ class SquadController extends Controller
         $user = Auth::user();
         $input = $request->all();
 
-        $squad = Squad::create([
-            "squad_name" => $input['squad_name'],
-            "game" => $input['game'],
-            "founder" => $user->username,
-            "recruiting" => $input['recruiting']
-        ]);
+        $existing_name = Squad::where('squad_name', $input['squad_name'])->get()->first();
 
-        $member = SquadMember::create([
-            "squad_id" => $squad->squad_id,
-            "id" => $user->id,
-            "squad_admin" => 1,
-        ]);
+        if ($existing_name) {
+            $response = array(
+                'success' => false,
+                'error' => "squad name already in use."
+            );
+            return response()->json($response, 200);
+        } else {
+            $squad = Squad::create([
+                "squad_name" => $input['squad_name'],
+                "game" => $input['game'],
+                "founder" => $user->username,
+                "recruiting" => $input['recruiting']
+            ]);
 
+            $member = SquadMember::create([
+                "squad_id" => $squad->squad_id,
+                "id" => $user->id,
+                "squad_admin" => 1,
+            ]);
 
-        $response = array(
-            'success' => $member ? true : false,
-            'data' => $member,
-            'error' => $member ? false : "failed to create squad"
-        );
+            $response = array(
+                'success' => $member ? true : false,
+                'data' => $member,
+                'error' => $member ? false : "failed to create squad"
+            );
 
-        return response()->json($response, 200);
+            return response()->json($response, 200);
+        }
     }
 }
