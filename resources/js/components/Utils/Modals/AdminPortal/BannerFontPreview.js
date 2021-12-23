2a1e5237361f8dbd7f76057518afe77787a80109
James <Thylinguistics@gmail.com>
2021-12-23 13:01:45 -0800

    squad logic

--- resources/js/components/Utils/Modals/AdminPortal/BannerFontPreview.js
+++ resources/js/components/Utils/Modals/AdminPortal/BannerFontPreview.js
@@ -18,9 +18,37 @@ const BannerFontPreview = ({ fontColor, fontFamily, squad }) => {
         }
     };
 
+    const renderBannerFontColor = () => {
+        if (fontColor) {
+            if (fontColor === "black") {
+                return "appearance-font-color-black";
+            }
+            if (fontColor === "red") {
+                return "appearance-font-color-red";
+            }
+            if (fontColor === "blue") {
+                return "appearance-font-color-blue";
+            }
+            if (fontColor === "green") {
+                return "appearance-font-color-green";
+            }
+            if (fontColor === "orange") {
+                return "appearance-font-color-orange";
+            }
+            if (fontColor === "pink") {
+                return "appearance-font-color-pink";
+            }
+            if (fontColor === "purple") {
+                return "appearance-font-color-purple";
+            }
+        }
+    };
+
     return (
         <div>
-            <h1 className={`banner-squad-name ${renderBannerFontFamily()} `}>
+            <h1
+                className={`banner-squad-name ${renderBannerFontFamily()} ${renderBannerFontColor()}`}
+            >
                 {squad ? squad.squad_name : ""}
             </h1>
         </div>
